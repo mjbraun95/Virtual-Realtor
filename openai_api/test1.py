d50432c633c50089926b89c64f5eb1f0bbd137b1
Matthew Braun <mjbraun@ualberta.ca>
2023-07-22 11:47:38 -0600

    Fixed  prompt

--- openai_api/test1.py
+++ openai_api/test1.py
@@ -3,18 +3,21 @@ import openai
 # You should get your API key from the OpenAI website.
 with open('api_key.txt', 'r') as file:
     openai.api_key = file.read().strip()
+
+user_input = input("Hello! I'm your virtual realtor. Tell me more about what you are looking for: ")
+
     
 # Start the conversation
 conversation = [
     {"role": "system", "content": "You are a realtor who assists buyers in finding properties that match their criteria and preferences. They provide information on available properties, schedule property viewings, and guide buyers throughout the purchasing process."},
-    {"role": "user", "content": "Translate the following English text to French: 'Hello, how are you?'"}
+    {"role": "user", "content": user_input}
 ]
 
 # Let's use the OpenAI GPT-3 model to generate some text.
-response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt="Translate the following English text to French: '{}'",
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=conversation,
   max_tokens=60
 )
 
-print(response.choices[0].text.strip())
+print(response['choices'][0]['message']['content'])
