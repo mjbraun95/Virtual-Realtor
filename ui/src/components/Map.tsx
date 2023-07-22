ab5884d42aeaea1806b582cac2f048738927446e
Aidan Olsen <a.olsen2024@icloud.com>
2023-07-22 12:10:00 -0600

    Run prettier

--- ui/src/components/Map.tsx
+++ ui/src/components/Map.tsx
@@ -1,7 +1,7 @@
 import { Card } from "@mui/material";
 
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 interface Home {
   id: number;
@@ -20,8 +20,7 @@ function Home({ home }: HomeProps) {
     <Marker position={{ lat: home.lat, lng: home.lng }}>
       <Popup>
         {home.address}
-        <br />
-        ${home.price}
+        <br />${home.price}
       </Popup>
     </Marker>
   );
@@ -48,7 +47,12 @@ export default function Map() {
 
   return (
     <Card sx={{ flex: 2, display: "block" }}>
-      <MapContainer style={{ width: "100%", height: "100%" }} center={position} zoom={11} scrollWheelZoom={false}>
+      <MapContainer
+        style={{ width: "100%", height: "100%" }}
+        center={position}
+        zoom={11}
+        scrollWheelZoom={false}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- ui/src/views/Home.tsx
+++ ui/src/views/Home.tsx
@@ -6,7 +6,14 @@ import Map from "../components/Map";
 export default function Home() {
   return (
     <Container maxWidth="lg">
-      <Stack display="flex" direction="row" height="95vh" width="100%" m={0} overflow="none">
+      <Stack
+        display="flex"
+        direction="row"
+        height="95vh"
+        width="100%"
+        m={0}
+        overflow="none"
+      >
         <Chat />
         <Map />
       </Stack>
