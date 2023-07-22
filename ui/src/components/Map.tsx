9fbceb61dce721199b62f10c5681e67bf1dbb03a
Aidan Olsen <a.olsen2024@icloud.com>
2023-07-22 12:03:33 -0600

    Add home listings to the map

--- ui/src/components/Map.tsx
+++ ui/src/components/Map.tsx
@@ -1,24 +1,62 @@
 import { Card } from "@mui/material";
 
 import "leaflet/dist/leaflet.css";
-import marker from "leaflet/dist/images/marker-icon.png";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+interface Home {
+  id: number;
+  address: string;
+  price: number;
+  lat: number;
+  lng: number;
+}
+
+interface HomeProps {
+  home: Home;
+}
+
+function Home({ home }: HomeProps) {
+  return (
+    <Marker position={{ lat: home.lat, lng: home.lng }}>
+      <Popup>
+        {home.address}
+        <br />
+        ${home.price}
+      </Popup>
+    </Marker>
+  );
+}
+
 export default function Map() {
-  const position = { lat: 51.505, lng: -0.09 };
+  const position = { lat: 53.55014, lng: -113.46871 };
+  const homes: Home[] = [
+    {
+      id: 0,
+      address: "441 That St.",
+      price: 325500,
+      lat: 53.45014,
+      lng: -113.46871,
+    },
+    {
+      id: 1,
+      address: "5231 102 Ave.",
+      price: 550000,
+      lat: 53.56014,
+      lng: -113.46881,
+    },
+  ];
 
   return (
     <Card sx={{ flex: 2, display: "block" }}>
-      <MapContainer style={{ width: "100%", height: "100%" }} center={position} zoom={13} scrollWheelZoom={false}>
+      <MapContainer style={{ width: "100%", height: "100%" }} center={position} zoom={11} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {homes.map((home) => (
+          <Home key={home.id} home={home} />
+        ))}
       </MapContainer>
     </Card>
   );
